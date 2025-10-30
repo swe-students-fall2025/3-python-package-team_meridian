c235b93ab5d46e3767dc674cd821c60949704f66
SinaL0123 <sl9608@nyu.edu>
2025-10-30 19:34:18 -0400

    Port files from origin/SinaL0123 onto pipfile-experiment base

---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+import pytest
+from pyfortunecookie.core import get_fortune, get_lucky_number, get_color
+
+def test_get_fortune():
+    result = get_fortune()
+    assert isinstance(result, str)
+    assert len(result) > 0
+
+def test_get_lucky_number():
+    num = get_lucky_number()
+    assert isinstance(num, int)
+    assert 1 <= num <= 99
+
+def test_get_color():
+    color = get_color()
+    assert isinstance(color, str)
+    assert len(color) > 0
