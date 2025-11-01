21a84d81859dfebe43c44fa29be08433a0abc826
DplayerXAX <danielhuang0905@outlook.com>
2025-11-01 15:37:50 -0400

    add tarot into main

--- src/pyfortunecookie/__main__.py
+++ src/pyfortunecookie/__main__.py
@@ -3,13 +3,15 @@ In Python packages, this file called __main__.py is run when the package is run
 directly from command line, as opposed to importing it into another program.
 """
 
-from pyfortunecookie.core import get_fortune, get_lucky_number, get_color
+from pyfortunecookie.core import get_fortune, get_lucky_number, get_color,get_tarot_reading
 
 def main():
     print("🥠 Welcome to PyFortune Cookie!\n")
     print(f"Today's fortune: {get_fortune()}")
     print(f"Your lucky number: {get_lucky_number()}")
     print(f"Your lucky color: {get_color()}")
+    print("\n🔮 Tarot Reading:")
+    print(get_tarot_reading())
 
 if __name__ == "__main__":
     main()
